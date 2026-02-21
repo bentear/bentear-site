37a64aff11e387a4ec340f979da5efd99c1eb1ae
Ben Tear <benjamin.tear@gmail.com>
2026-02-20 16:14:23 -0800

    add close button for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -145,6 +145,13 @@ export default function Home() {
               <source src="/reel.mp4" type="video/mp4" />
             </video>
             
+            {/* Close Button - Mobile only */}
+            <button className="close-btn" onClick={closeModal}>
+              <svg viewBox="0 0 24 24">
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+              </svg>
+            </button>
+            
             {/* Mute Button */}
             <button className="mute-btn" onClick={toggleMute}>
               {isMuted ? (
